1452da8ea8f92fd12f34775221c44d3569a591a2
Mark Ellis <markellisdev@gmail.com>
2017-03-16 06:07:01 -0500

    login controller and view, still buggy

--- devdactic-simpleLogin/src/pages/login/login.ts
+++ devdactic-simpleLogin/src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, LoadingController, Loading} from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
 
 /*
   Generated class for the Login page.
@@ -12,8 +15,51 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  loading: Loading;
+  registerCredentials = {email: '', password: ''};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController, private loadingCtrl: LoadingController) {}
+
+  public createAccount() {
+    this.nav.push(RegisterPage);
+  }
+
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {
+        setTimeout(() => {
+          this.loading.dismiss();
+          this.nav.setRoot(HomePage)
+        });
+      } else {
+        this.showError("Access Denied");
+      }
+    },
+    error => {
+      this.showError(error);
+    });
+  }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+
+  showError(text) {
+    setTimeout(() => {
+      this.loading.dismiss();
+    });
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt)
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
--- devdactic-simpleLogin/src/providers/auth-service.ts
+++ devdactic-simpleLogin/src/providers/auth-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 /*
@@ -8,8 +9,57 @@ import 'rxjs/add/operator/map';
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
+export class User {
+  name: string;
+  email: string;
+
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
 @Injectable()
 export class AuthService {
+  currentUser: User;
+
+  public login(credentials) {
+    if (credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+      return Observable.create(observer => {
+        // At this point make a request to your backend to make a real check!
+        let access = (credentials.password === "pass" && credentials.email === "email")
+        this.currentUser = newUser('Mark', 'me@me.com');
+        observer.next(access);
+        observer.complete();
+      });
+    }
+  }
+
+  public register(credentials) {
+    if (credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+      // At this point store credentials to your backend!
+      return Observable.create(observer => {
+        observer.next(true);
+        observer.complete();
+      });
+    }
+  }
+
+  public.getUserInfo() : User {
+    return this.currentUser;
+  }
+
+  public logout() {
+    return Observable.create(observer => {
+      this.currentUser = null;
+      observer.next(true);
+      observer.complete();
+    });
+  }
 
   constructor(public http: Http) {
     console.log('Hello AuthService Provider');
